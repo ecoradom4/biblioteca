6176288fa107ac837cd0077e874abe5860a575ec
Eduardo <eduardo@debian.eduardo>
2025-08-05 17:26:37 -0600

    ssl

--- app/models/index.js
+++ app/models/index.js
@@ -4,6 +4,12 @@ const dbConfig = require('../config/db.config');
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
